a7164dd0bf8993e63055d3531a4c28349b9eee9d
yourash <y.anton.ua@gmail.com>
2018-11-24 22:58:07 +0200

    Add schools

---
+++ distance.py
@@ -0,0 +1,30 @@
+import requests
+from config import TOM_TOM_API_KEY
+
+
+def get_closest_school(lat, lon):
+    """
+    :param lat: latitude
+    :param lon: longitude
+    :return: [5] results ['poi']['name']/['freeformAddress'] || ['position']['lat']/['lon']
+    """
+    results = []
+    r = requests.get('https://api.tomtom.com/search/2/poiSearch/school'
+                     '.json?key={0}&lat={1}&lon={2}&radius=10000&limit=100'.format(
+        TOM_TOM_API_KEY,
+        lat,
+        lon
+    ))
+    for result in r.json()['results']:
+        if result['poi']['categories'] == ['school'] and \
+                'садок' not in result['poi']['name'] and \
+                '1S' not in result['poi']['name'] and \
+                'Khammer' not in result['poi']['name']:
+            result.append(results)
+        if len(results) == 5:
+            break
+    return results
+
+
+if __name__ == '__main__':
+    get_closest_school(50.4654669, 30.3745462)
