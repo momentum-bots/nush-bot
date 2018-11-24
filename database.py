9919fdfc1b3bb676cabe623f0e2828f8d573fe07
yourash <y.anton.ua@gmail.com>
2018-11-24 20:39:56 +0200

    Add cfg setup

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from mongoengine import *
 from config import DATABASE_HOST, DATABASE_LOGIN, DATABASE_NAME, DATABASE_PASSWORD, DATABASE_PORT
 
-connect(DATABASE_NAME)
+connect(DATABASE_NAME, host=DATABASE_HOST, port=DATABASE_PORT, username=DATABASE_LOGIN, password=DATABASE_PASSWORD)
 print('Connected to {0}'.format(DATABASE_NAME))
 
 LANGUAGES = ['ua']
